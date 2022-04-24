3e19aee409aa88ff72cbc108e136273594e976bd
BenjiChejtman <46895375@est.ort.edu.ar>
2022-04-24 14:08:52 -0300

    Done

--- Assets/Ej_1.cs
+++ Assets/Ej_1.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Ej_1 : MonoBehaviour
 {
+
+    int counter = 1;
+    int counterMax = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        while (counter <= counterMax)
+        {
+            Debug.Log(counter);
+            counter++;
+        }
+        Debug.Log("Termino el while.");
     }
 
     // Update is called once per frame
